451c0d8aed93b2af7ef4caaa3320df5707483394
abduladi2423 <146325218+abduladi2423@users.noreply.github.com>
2025-01-02 00:28:14 +0500

    The Lookup Class file

---
+++ Lookups.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test.Models;
+
+namespace test
+{
+    public static class Lookups
+    {
+        public static List<Dishes> lstDishes { get; set; }
+    }
+}
