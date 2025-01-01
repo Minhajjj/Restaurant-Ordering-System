4d62b229623d2cd29193e7c2b7eea1145a62c642
Minhajjj <146324676+Minhajjj@users.noreply.github.com>
2025-01-01 23:34:32 +0500

    Adding files of Cart

---
+++ Cart.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace test.Forms
+{
+    partial class Cart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cart));
+            label2 = new Label();
+            CartGridView = new DataGridView();
+            dishPricetxt = new DataGridViewTextBoxColumn();
+            colAction = new DataGridViewButtonColumn();
+            dishNametxt = new DataGridViewTextBoxColumn();
+            categorytxt = new DataGridViewTextBoxColumn();
+            dishesBindingSource = new BindingSource(components);
+            btnClearCart = new Button();
+            btnUpdateCart = new Button();
+            Paymentbtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)CartGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dishesBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Script MT Bold", 28F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.ForeColor = Color.Moccasin;
+            label2.Location = new Point(755, 20);
+            label2.Name = "label2";
+            label2.Size = new Size(287, 67);
+            label2.TabIndex = 1;
+            label2.Text = "Your Cart :";
+            // 
+            // CartGridView
+            // 
+            CartGridView.AutoGenerateColumns = false;
+            CartGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            CartGridView.BackgroundColor = Color.DeepSkyBlue;
+            CartGridView.BorderStyle = BorderStyle.Fixed3D;
+            CartGridView.CellBorderStyle = DataGridViewCellBorderStyle.SunkenVertical;
+            CartGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            CartGridView.Columns.AddRange(new DataGridViewColumn[] { dishPricetxt, colAction, dishNametxt, categorytxt });
+            CartGridView.Cursor = Cursors.Hand;
+            CartGridView.DataSource = dishesBindingSource;
+            CartGridView.GridColor = Color.Khaki;
+            CartGridView.Location = new Point(189, 90);
+            CartGridView.Name = "CartGridView";
+            CartGridView.RowHeadersWidth = 62;
+            CartGridView.RowTemplate.Height = 33;
+            CartGridView.Size = new Size(1359, 325);
+            CartGridView.TabIndex = 2;
+            CartGridView.CellContentClick += CartGridView_CellContentClick;
+            // 
+            // dishPricetxt
+            // 
+            dishPricetxt.DataPropertyName = "DishPrice";
+            dishPricetxt.HeaderText = "DishPrice";
+            dishPricetxt.MinimumWidth = 8;
+            dishPricetxt.Name = "dishPricetxt";
+            // 
+            // colAction
+            // 
+            colAction.HeaderText = "Action";
+            colAction.MinimumWidth = 8;
+            colAction.Name = "colAction";
+            colAction.Text = "Remove items";
+            colAction.UseColumnTextForButtonValue = true;
+            // 
+            // dishNametxt
+            // 
+            dishNametxt.DataPropertyName = "DishName";
+            dishNametxt.HeaderText = "DishName";
+            dishNametxt.MinimumWidth = 8;
+            dishNametxt.Name = "dishNametxt";
+            // 
+            // categorytxt
+            // 
+            categorytxt.DataPropertyName = "Category";
+            categorytxt.HeaderText = "Category";
+            categorytxt.MinimumWidth = 8;
+            categorytxt.Name = "categorytxt";
+            // 
+            // dishesBindingSource
+            // 
+            dishesBindingSource.DataSource = typeof(Models.Dishes);
+            // 
+            // btnClearCart
+            // 
+            btnClearCart.BackColor = Color.LawnGreen;
+            btnClearCart.Font = new Font("Franklin Gothic Demi", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnClearCart.Location = new Point(1613, 445);
+            btnClearCart.Name = "btnClearCart";
+            btnClearCart.Size = new Size(112, 34);
+            btnClearCart.TabIndex = 3;
+            btnClearCart.Text = "Clear Cart";
+            btnClearCart.UseVisualStyleBackColor = false;
+            btnClearCart.Click += btnClearCart_Click;
+            // 
+            // btnUpdateCart
+            // 
+            btnUpdateCart.Location = new Point(0, 0);
+            btnUpdateCart.Name = "btnUpdateCart";
+            btnUpdateCart.Size = new Size(75, 23);
+            btnUpdateCart.TabIndex = 8;
+            // 
+            // Paymentbtn
+            // 
+            Paymentbtn.BackColor = Color.Yellow;
+            Paymentbtn.Font = new Font("Franklin Gothic Demi Cond", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            Paymentbtn.Location = new Point(1483, 445);
+            Paymentbtn.Name = "Paymentbtn";
+            Paymentbtn.Size = new Size(112, 34);
+            Paymentbtn.TabIndex = 7;
+            Paymentbtn.Text = "Payment";
+            Paymentbtn.UseVisualStyleBackColor = false;
+            Paymentbtn.Click += Paymentbtn_Click;
+            // 
+            // Cart
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1747, 491);
+            Controls.Add(Paymentbtn);
+            Controls.Add(btnUpdateCart);
+            Controls.Add(btnClearCart);
+            Controls.Add(CartGridView);
+            Controls.Add(label2);
+            Name = "Cart";
+            Text = "Cart";
+            Load += Cart_Load;
+            ((System.ComponentModel.ISupportInitialize)CartGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dishesBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+        private DataGridView CartGridView;
+        private BindingSource dishesBindingSource;
+        private Button btnClearCart;
+        private Button btnUpdateCart;
+        private Button Paymentbtn;
+        private DataGridViewTextBoxColumn dishPricetxt;
+        private DataGridViewButtonColumn colAction;
+        private DataGridViewTextBoxColumn dishNametxt;
+        private DataGridViewTextBoxColumn categorytxt;
+    }
+}
---
+++ Cart.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test.Models;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace test.Forms
+{
+    public partial class Cart : Form
+    {
+
+        BindingList<Dishes> lst;
+        public Cart(BindingList<Dishes> selectedDishes)
+        {
+
+            lst = selectedDishes;
+            InitializeComponent();
+            CartGridView.DataSource = selectedDishes;
+
+
+            CartGridView.Columns["dishNametxt"].DisplayIndex = 0;
+            CartGridView.Columns["categorytxt"].DisplayIndex = 1;
+            CartGridView.Columns["dishPricetxt"].DisplayIndex = 2;
+            CartGridView.Columns["colAction"].DisplayIndex = 3;
+
+
+
+        }
+
+        private void Cart_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CartGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView cvg = CartGridView as DataGridView;
+            var selectedRow = CartGridView.Rows[e.RowIndex];
+            var dish = selectedRow.DataBoundItem as Dishes; // Assuming DataGridView is bound to a data source
+            if (cvg.Columns[e.ColumnIndex].Name == "colAction")
+            {
+                if (dish != null)
+                {
+                    CartGridView.Rows.RemoveAt(e.RowIndex);
+                    var itemToRemove = lst.FirstOrDefault(x => x.id == dish.id);
+                    lst.Remove(itemToRemove);
+                }
+            }
+            // Add the dish to the cart (you can dsefine your AddToCart method)
+
+        }
+
+        private void btnClearCart_Click(object sender, EventArgs e)
+        {
+
+            CartGridView.Rows.Clear();
+            lst.Clear();
+            MessageBox.Show("Cart has been cleared!", "Clear Cart", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void Paymentbtn_Click(object sender, EventArgs e)
+        {
+            Bill billForm = new Bill(lst);
+
+            // Show the Bill form
+            billForm.Show();
+        }
+
+
+    }
+}
